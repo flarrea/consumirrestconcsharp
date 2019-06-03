ae2893ace5d7a15912e619ff695a9cdfd4b14c96
flarrea <flarreas@gmail.com>
2019-06-03 09:53:52 -0400

    demo consumir un servicio REST en Firebase desde C# v1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+//instalo desde la consola: dotnet add package Newtonsoft.Json
+
+namespace myRest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://localizacajero.firebaseio.com/cajeros.json";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            
+            //Console.WriteLine(json); Retorna el objeto
+                                 
+            foreach(var i in m){
+                Console.WriteLine(i.direccion);
+            }                       
+        }
+    }
+}
